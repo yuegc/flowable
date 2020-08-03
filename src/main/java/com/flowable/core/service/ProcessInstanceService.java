4c984f8260a733085ecfb13f83389b44379440bd
yuegc <yuegc@weherepost.com>
2020-08-03 14:57:40 +0800

    流程实例接口定义

--- src/main/java/com/flowable/core/service/ProcessInstanceService.java
+++ src/main/java/com/flowable/core/service/ProcessInstanceService.java
@@ -6,4 +6,13 @@ package com.flowable.core.service;
  * @create: 2020/01/19 17:44
  */
 public interface ProcessInstanceService {
+    void processInstanceList();
+
+    void startProcessInstance();
+
+    void stopProcessInstance();
+
+    void deleteProcessInstance();
+
+    void suspendProcessInstance();
 }
--- src/main/java/com/flowable/core/service/impl/ProcessInstanceServiceImpl.java
+++ src/main/java/com/flowable/core/service/impl/ProcessInstanceServiceImpl.java
@@ -8,4 +8,28 @@ import com.flowable.core.service.ProcessInstanceService;
  * @create: 2020/07/15 10:49
  */
 public class ProcessInstanceServiceImpl implements ProcessInstanceService {
+    @Override
+    public void processInstanceList() {
+
+    }
+
+    @Override
+    public void startProcessInstance() {
+
+    }
+
+    @Override
+    public void stopProcessInstance() {
+
+    }
+
+    @Override
+    public void deleteProcessInstance() {
+
+    }
+
+    @Override
+    public void suspendProcessInstance() {
+
+    }
 }
