94e46ee1b8cf12db9d0ba3ceb35453f41838449c
yuegc <yuegc@weherepost.com>
2020-08-04 10:14:08 +0800

    新增异常类

---
+++ src/main/java/com/flowable/core/enums/FlowbleEnum.java
@@ -0,0 +1,19 @@
+package com.flowable.core.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * @author: yuegc
+ * @description:
+ * @create: 2020/08/04 09:45
+ */
+@Getter
+@AllArgsConstructor
+public enum FlowbleEnum {
+    SUCCESS(1, "");
+
+    private final int code;
+
+    private final String msg;
+}
